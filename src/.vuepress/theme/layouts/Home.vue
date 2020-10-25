25e42ffb0aa4d4d73b6d2878722f99a2b4cb96f4
Jose Carlos Urra <jose.urra86@gmail.com>
2020-10-25 22:02:20 +0100

    Added some more styling, logo and stuff

---
+++ src/.vuepress/theme/layouts/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Nav />
+    <template>
+      <slot />
+    </template>
+    <Footer />
+  </div>
+</template>
+
+<static-query>
+  query {
+    metadata {
+              siteName
+    }
+  }
+</static-query>
+
+<script>
+import Nav from "~/components/Nav.vue";
+import Footer from "~/components/Footer";
+
+export default {
+  components: {
+    Nav,
+    Footer
+  }
+};
+</script>
+
+<style>
+footer {
+  justify-self: flex-start;
+}
+</style>
